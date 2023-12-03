4fd0aae695b661cf43825087119ae1fea79a8c58
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-03 10:59:42 -0300

    refactor: extract red rectangle
    change rgb 240 to 200

--- opencv_extract_rectangle_area_image_api/src/extract_red_rectangle.py
+++ opencv_extract_rectangle_area_image_api/src/extract_red_rectangle.py
@@ -5,7 +5,7 @@ def extract_red_rectangle(image_path: str) -> dict[str, int]:
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    red_range_init = np.array([240, 0, 0], dtype=np.uint8)
+    red_range_init = np.array([200, 0, 0], dtype=np.uint8)
     red_range_end = np.array([255, 0, 0], dtype=np.uint8)
 
     mask = cv2.inRange(image_rgb, red_range_init, red_range_end)
