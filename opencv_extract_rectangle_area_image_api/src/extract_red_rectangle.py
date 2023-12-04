d6310777a06fd63fcb638963d584a7dc188d26ea
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-04 12:00:38 -0300

    refactor: extract red rectangle
    change methods to extract to update extract area

--- opencv_extract_rectangle_area_image_api/src/extract_red_rectangle.py
+++ opencv_extract_rectangle_area_image_api/src/extract_red_rectangle.py
@@ -1,26 +1,36 @@
 import cv2
 import numpy as np
 
+
 def extract_red_rectangle(image_path: str) -> dict[str, int]:
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    red_range_init = np.array([200, 0, 0], dtype=np.uint8)
-    red_range_end = np.array([255, 0, 0], dtype=np.uint8)
+    image_hsv = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2HSV)
 
-    mask = cv2.inRange(image_rgb, red_range_init, red_range_end)
+    # Define the red range
+
+    lower_red1 = np.array([0, 100, 100], dtype=np.uint8)
+    upper_red1 = np.array([10, 255, 255], dtype=np.uint8)
+
+    lower_red2 = np.array([160, 100, 100], dtype=np.uint8)
+    upper_red2 = np.array([180, 255, 255], dtype=np.uint8)
+
+    # Combine all red ranges
+    mask1 = cv2.inRange(image_hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(image_hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+
+    # Apply a morphology filter to upgrade the detection
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
         max_contour = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(max_contour)
-        return {
-            'x': x, 
-            'y': y, 
-            'w': w, 
-            'h': h
-        }
-    
+        return {"x": x, "y": y, "w": w, "h": h}
+
     else:
-        raise Exception('Red rectangle not found')
+        raise Exception("Red rectangle not found")
